2576452a8b3f60962877a4bfb2081c25ca34f61a
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2018-02-05 11:20:19 +0300

    Uast: fixes for non-leaf identifiers

--- plugins/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinUastLanguagePlugin.kt
+++ plugins/uast-kotlin/src/org/jetbrains/uast/kotlin/KotlinUastLanguagePlugin.kt
@@ -290,6 +290,8 @@ internal object KotlinConverter {
         else -> element
     }
 
+    private val identifiersTokens = setOf(KtTokens.IDENTIFIER, KtTokens.CONSTRUCTOR_KEYWORD, KtTokens.THIS_KEYWORD, KtTokens.SUPER_KEYWORD)
+
     internal fun convertPsiElement(element: PsiElement?,
                                    givenParent: UElement?,
                                    requiredType: Class<out UElement>?): UElement? {
@@ -347,7 +349,7 @@ internal object KotlinConverter {
             is KtImportDirective -> el<UImportStatement>(build(::KotlinUImportStatement))
             else -> {
                 if (element is LeafPsiElement) {
-                    if (element.elementType == KtTokens.IDENTIFIER || element.elementType == KtTokens.CONSTRUCTOR_KEYWORD)
+                    if (element.elementType in identifiersTokens)
                     el<UIdentifier>(build(::KotlinUIdentifier))
                     else if (element.elementType == KtTokens.LBRACKET && element.parent is KtCollectionLiteralExpression)
                         el<UIdentifier> {
